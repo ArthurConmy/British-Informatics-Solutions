55109d0116bf1ee703b850b85eb9aa6e1ea98877
ArthurConmy <35957051+ArthurConmy@users.noreply.github.com>
2020-06-29 21:47:00 +0100

    Create e.cpp

---
+++ solutions/codeforces/1073/e.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define len(x) int((x).size())
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define randint(n) uniform_int_distribution<int>(1, (n))(rng)
+
+int k;
+
+int solve(ll n)
+{
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	#ifdef ARTHUR_LOCAL
+		ifstream cin("input.txt");
+	#endif
+
+	ll l,r;
+	cin>>l>>r>>k;
+
+	int 
+}
